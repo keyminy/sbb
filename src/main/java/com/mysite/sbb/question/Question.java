c9b324f935876bbf96118dc7c18b063e8e63fac8
keyminy <imanaifs@gmail.com>
2023-01-20 15:55:59 +0900

    basic controller

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,13 +1,17 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.mysite.sbb.answer.Answer;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +20,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Answer {
+public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
+    @Column(length = 200)
+    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
-
-    @ManyToOne
-    private Question question;
+    
+    @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
