ce58ae3597eb09b2c9dbbd5686405371ba6cdeea
keyminy <imanaifs@gmail.com>
2023-01-30 15:21:09 +0900

    3-12,앵커태그로 이동

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -44,8 +44,10 @@ public class AnswerController {
             return "question_detail";
         }
 		//Answer저장(this.answerRepository.save(answer);)
-		this.answerService.create(question, answerForm.getContent(),siteUser);
-		return String.format("redirect:/question/detail/%s",id);
+        Answer answer = this.answerService.create(question
+        		,answerForm.getContent(),siteUser);
+		return String.format("redirect:/question/detail/%s#answer_%s"
+				,answer.getQuestion().getId(),answer.getId());
 	 }
 	
 	@PreAuthorize("isAuthenticated()")
@@ -73,7 +75,8 @@ public class AnswerController {
 	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
 	       }
 		  this.answerService.modify(answer, answerForm.getContent());
-	      return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+	      return String.format("redirect:/question/detail/%s#answer_%s"
+	    		  , answer.getQuestion().getId(),answer.getId());
 	  }
 	  
 	  @PreAuthorize("isAuthenticated()")
@@ -84,7 +87,8 @@ public class AnswerController {
 			  throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
 	      }
 		 this.answerService.delete(answer);
-	     return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+	     return String.format("redirect:/question/detail/%s#answer_%s"
+	    		 , answer.getQuestion().getId(),answer.getId());
 	  }
 	  
 	@PreAuthorize("isAuthenticated()")
@@ -93,6 +97,7 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s"
+        		, answer.getQuestion().getId(),answer.getId());
     }
 }
